823062629187dee08fb8d88c6a75ed2bd9533eed
Meshach Mitchell <rain@mawu.home>
2019-01-29 13:38:33 -0500

    database initial setup

--- src/pantologic/settings.py
+++ src/pantologic/settings.py
@@ -75,6 +75,10 @@ WSGI_APPLICATION = 'pantologic.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE'   : 'django.db.backends.postgresql',
+        'NAME'     : 'pantologic',
+    },
+    'fallback': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }

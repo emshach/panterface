1998a1eb0e1fe93763c11efc7d8a0089a496869d
Ponder Stibbons <theponderstibbons@gmail.com>
2019-02-15 15:54:35 -0500

    fix app route

--- src/pantologic/urls.py
+++ src/pantologic/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url( r'^admin/', admin.site.urls ),
     url( r'^intrepid/', include( 'intrepid.urls' )),
     url( r'^api-auth/', include( 'rest_framework.urls' )),
+    url( r'^admin/', admin.site.urls ),
 ]

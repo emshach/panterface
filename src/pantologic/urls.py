060e73e0c380587eceb57821b2c912308b0e3ea6
Ponder Stibbons <theponderstibbons@gmail.com>
2019-02-15 15:41:45 -0500

    add app intrepid

--- src/pantologic/urls.py
+++ src/pantologic/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url( r'^admin/', admin.site.urls ),
+    url( r'^intrepid/', include( 'intrepid.urls' )),
     url( r'^api-auth/', include( 'rest_framework.urls' )),
 ]

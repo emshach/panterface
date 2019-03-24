cac7e227905054a8d85bf1ed7cdff570735bdb93
Ponder Stibbons <theponderstibbons@gmail.com>
2019-03-23 21:45:09 -0500

    Fix missing import

--- src/intrepid/serializers.py
+++ src/intrepid/serializers.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from rest_framework import serializers
-
+from .model import *
 
 class NoteSerializer( serializers.HyperlinkedModelSerializer ):
     class Meta:

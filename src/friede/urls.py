cd9140d7a5fdbe959a264333c10d4c5fca3c86ab
Ponder Stibbons <theponderstibbons@gmail.com>
2019-04-07 18:12:22 -0500

    Remember trailing slash for routes

--- src/friede/urls.py
+++ src/friede/urls.py
@@ -33,7 +33,7 @@ try:
 except Exception:
     # pass                        # TODO: handle
     raise
-urlpatterns.extend([ url( r"^api/%s" % k, include( v.urls , namespace='friede' ))
+urlpatterns.extend([ url( r"^api/%s/" % k, include( v.urls , namespace='friede' ))
                      for k, v in routes.items() ])
 urlpatterns.append( url( r'^api/', views.api_root ))
 urlpatterns.append( url( r'^.*', views.index, name='index' ))

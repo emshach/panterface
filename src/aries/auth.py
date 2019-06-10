750fc524574bb786746fa785b84126d7c6e3d96b
Ponder Stibbons <theponderstibbons@gmail.com>
2019-06-10 12:31:22 -0500

    Fix bad code indentation

--- src/aries/auth.py
+++ src/aries/auth.py
@@ -49,8 +49,8 @@ class AnonymousAuthBackend:
             user = None
         return user
 
-        def get_user( self, user_id ):
-            try:
-                return User.objects.get( pk=user_id )
-            except User.DoesNotExist:
-                return None
+    def get_user( self, user_id ):
+        try:
+            return User.objects.get( pk=user_id )
+        except User.DoesNotExist:
+            return None

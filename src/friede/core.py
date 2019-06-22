e2959352df7e5c630d0b4428b316233b2027e899
Ponder Stibbons <theponderstibbons@gmail.com>
2019-06-22 10:11:35 -0500

    Fix bad var name

--- src/friede/core.py
+++ src/friede/core.py
@@ -192,7 +192,7 @@ def updateapp( name, data=None, obj=None ):
 
         print path, v, 'available'
 
-        app = obj.model if obs and obj.model else App.objects.get( path=path )
+        app = obj.model if obj and obj.model else App.objects.get( path=path )
         app.available = str( available )
         if obj:
             app.min_version = obj.min_version

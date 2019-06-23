7585c11d7116c21101d8d5040e3b8cee31d2dae1
Ponder Stibbons <theponderstibbons@gmail.com>
2019-06-23 01:04:04 -0500

    Automatically make permissions for aries owned models

--- src/aries/models.py
+++ src/aries/models.py
@@ -5,6 +5,7 @@ from django.db import models as M
 from django.contrib.postgres.fields import JSONField
 from django.contrib.auth import models as auth
 from django.db.models.signals import post_save
+from django.contrib.contenttypes.models import ContentType
 
 Model = M.Model
 
@@ -101,6 +102,33 @@ class _AutoOwnedBase( M.base.ModelBase ):
 
         if model._meta.abstract or model._meta.concrete_model is not model:
             return model
+        name = model._meta.model_name
+        verbose = model._meta.verbose_name
+        ct = ContentType.objects.get_for_model( model )
+        Permission.objects.get_or_create(
+            defaults=dict( name="Can change own {}".format( verbose )),
+            codename="change_own_{}".format( name ),
+            content_type=ct )
+        Permission.objects.get_or_create(
+            defaults=dict( name="Can delete own {}".format( verbose )),
+            codename="delete_own_{}".format( name ),
+            content_type=ct )
+        Permission.objects.get_or_create(
+            defaults=dict( name="Can see {}".format( verbose )),
+            codename="see_{}".format( name ),
+            content_type=ct )
+        Permission.objects.get_or_create(
+            defaults=dict( name="Can see own {}".format( verbose )),
+            codename="see_own_{}".format( name ),
+            content_type=ct )
+        Permission.objects.get_or_create(
+            defaults=dict( name="Can view {}".format( verbose )),
+            codename="view_{}".format( name ),
+            content_type=ct )
+        Permission.objects.get_or_create(
+            defaults=dict( name="Can view own {}".format( verbose )),
+            codename="view_own_{}".format( name ),
+            content_type=ct )
         return model
 
 

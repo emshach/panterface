5703d16b832b1cb55587a8856c693c207021da90
Ponder Stibbons <theponderstibbons@gmail.com>
2019-06-24 12:08:34 -0500

    Fix bad class name

--- src/friede/urls.py
+++ src/friede/urls.py
@@ -43,7 +43,7 @@ except Exception:
 
 urlpatterns += [ u for x in tuple (
     ( url( r"^api/%s/" % k, include( v.urls )),
-      url( r"^api/%s" % k, Redirect.as_view( url=reverse_lazy(
+      url( r"^api/%s" % k, RedirectView.as_view( url=reverse_lazy(
           "api-root-%s" % k, permanent=True )))) if hasattr( v, 'urls' )
     else ( url( r"^api/%s" % k, v[0], name=v[1] ), )
     for k, v in routes.items() )

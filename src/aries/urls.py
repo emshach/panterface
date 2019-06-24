ab76d3bb2aedd49b4d8e6d79591ec52115b96275
Ponder Stibbons <theponderstibbons@gmail.com>
2019-06-23 21:40:31 -0500

    Move aries permissions creation to aries urls

---
+++ src/aries/urls.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.conf.urls import url, include
+from django.contrib.auth import get_user_model
+from django.contrib.contenttypes.models import ContentType
+from .views import login, logout, register
+from .models import owned, Permission
+
+for model in owned:
+    name = model._meta.model_name
+    verbose = model._meta.verbose_name
+    ct = ContentType.objects.get_for_model( model )
+    Permission.objects.get_or_create(
+        defaults=dict( name="Can change own {}".format( verbose )),
+        codename="change_own_{}".format( name ),
+        content_type=ct )
+    Permission.objects.get_or_create(
+        defaults=dict( name="Can delete own {}".format( verbose )),
+        codename="delete_own_{}".format( name ),
+        content_type=ct )
+    Permission.objects.get_or_create(
+        defaults=dict( name="Can see {}".format( verbose )),
+        codename="see_{}".format( name ),
+        content_type=ct )
+    Permission.objects.get_or_create(
+        defaults=dict( name="Can see own {}".format( verbose )),
+        codename="see_own_{}".format( name ),
+        content_type=ct )
+    Permission.objects.get_or_create(
+        defaults=dict( name="Can view {}".format( verbose )),
+        codename="view_{}".format( name ),
+        content_type=ct )
+    Permission.objects.get_or_create(
+        defaults=dict( name="Can view own {}".format( verbose )),
+        codename="view_own_{}".format( name ),
+        content_type=ct )
+
+urlpatterns = [
+    # url( r'^register/', login, name='register' ),
+    # url( r'^login/', login, name='login' ),
+    # url( r'^logout/', login, name='logout' ),
+]
--- src/pantologic/urls.py
+++ src/pantologic/urls.py
@@ -15,43 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from django.contrib.auth import get_user_model
-
-# init aries permissions FIXME: feels hackish
-from aries.models import owned, Permission
-from django.contrib.contenttypes.models import ContentType
-for model in owned:
-    name = model._meta.model_name
-    verbose = model._meta.verbose_name
-    ct = ContentType.objects.get_for_model( model )
-    Permission.objects.get_or_create(
-        defaults=dict( name="Can change own {}".format( verbose )),
-        codename="change_own_{}".format( name ),
-        content_type=ct )
-    Permission.objects.get_or_create(
-        defaults=dict( name="Can delete own {}".format( verbose )),
-        codename="delete_own_{}".format( name ),
-        content_type=ct )
-    Permission.objects.get_or_create(
-        defaults=dict( name="Can see {}".format( verbose )),
-        codename="see_{}".format( name ),
-        content_type=ct )
-    Permission.objects.get_or_create(
-        defaults=dict( name="Can see own {}".format( verbose )),
-        codename="see_own_{}".format( name ),
-        content_type=ct )
-    Permission.objects.get_or_create(
-        defaults=dict( name="Can view {}".format( verbose )),
-        codename="view_{}".format( name ),
-        content_type=ct )
-    Permission.objects.get_or_create(
-        defaults=dict( name="Can view own {}".format( verbose )),
-        codename="view_own_{}".format( name ),
-        content_type=ct )
 
 urlpatterns = [
     url( r'^api-auth/', include( 'rest_framework.urls', namespace='rest_framework' )),
     # url( r'^accounts/', include( 'allauth.urls' )),
     url( r'^admin/',    admin.site.urls ),
+    url( r'',        include( 'aries.urls', namespace='aries' )),
     url( r'',        include( 'friede.urls', namespace='friede' )),
 ]

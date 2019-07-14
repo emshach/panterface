6f297e33831386cc1e1325a0713ec7f4bee225f2
Ponder Stibbons <theponderstibbons@gmail.com>
2019-07-14 14:29:26 -0500

    Fix friede permit

--- src/friede/aries_permit.py
+++ src/friede/aries_permit.py
@@ -1,10 +1,10 @@
 from aries import permit
 
 def _friede_model( ct, data={} ):
-    return 'friede.'+ct
+    return 'friede.' + ct[-1]
 
 def _perm_name( name, data={} ):
-    return "Can {} {}".format(*( name[:2] ))
+    return "Can " + ' '.join( name )
 
 class Permit( permit.Permit ):
     name = 'friede'
@@ -13,13 +13,13 @@ class Permit( permit.Permit ):
       ( '#groups', 'anonymous authenticated confirmed verified',
         ( '#permissions',
           ( 'install uninstall update activate deactivate', 'own', 'app',
-            dict( ct='fride.app', name=_perm_name )))),
+            dict( ct='friede.app', name=_perm_name )))),
       ( '#roles',
         ( 'manager',
           ( 'app', dict( title='App Manager' ),
             ( '#permissions',
               ( 'install uninstall update activate deactivate', 'app',
-                dict( ct='fride.app', name=_perm_name )))),
+                dict( ct='friede.app', name=_perm_name )))),
           ( 'site', dict( title='Site Manager' ),
             ( '#permissions',
               ( 'add change delete',

232cdcf85e47e034e83fc134b6bbba13c50f1b1a
Ponder Stibbons <theponderstibbons@gmail.com>
2019-07-28 07:35:34 +0000

    Hack initial friede migration to fix rollback error

--- src/friede/migrations/0001_initial.py
+++ src/friede/migrations/0001_initial.py
@@ -372,7 +372,7 @@ class Migration(migrations.Migration):
                 ('registry_ptr', models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, serialize=False, to='friede.Registry')),
                 ('data', django.contrib.postgres.fields.jsonb.JSONField(default=dict)),
                 ('href', models.CharField(default='#', max_length=255)),
-                ('app', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='friede.App')),
+                ('app', models.ForeignKey(null=True,blank=True,on_delete=django.db.models.deletion.CASCADE, to='friede.App')),
                 ('redirect_to', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, related_name='redirect_from', to='friede.Location')),
             ],
             options={

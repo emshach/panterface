0779b867540612150f1178699f5c4188831ae31d
Ponder Stibbons <theponderstibbons@gmail.com>
2020-02-02 00:15:20 -0500

    Add friede test, first try

--- src/friede/tests.py
+++ src/friede/tests.py
@@ -2,5 +2,18 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
+from actions import Action, actions
+from .app import setup
+import friede.models as M
 
-# Create your tests here.
+
+class ActionsTestCase( TestCase ):
+    def setUp( self ):
+        setup([])
+
+    def test_can_get_action_object( self ):
+        actionobj = actions.get( 'install' )
+        actionstore = M.Action.objects.get( name='install' )
+        self.assertIsNotNone( actionobj )
+        self.assertIsNotNone( actionstore )
+        self.assertIs( actionobj, Action.get_for_object( actionstore ))

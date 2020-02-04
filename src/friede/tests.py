7d953c8b70308de30f06134cea0f65e4a1fe760f
Ponder Stibbons <theponderstibbons@gmail.com>
2020-02-04 11:15:17 -0500

    Add test for base app install

--- src/friede/tests.py
+++ src/friede/tests.py
@@ -13,15 +13,26 @@ class ActionsTestCase( TestCase ):
 
     def test_Can_retrieve_action_object_by_name( self ):
         actionobj = actions.get( 'install' )
-        self.assertIsNotNone( actionobj )
+        self.assertIsNotNone( actionobj, "Install action exists" )
 
     def test_Can_retrieve_action_store_by_name( self ):
         actionstore = M.Action.objects.get( name='install' )
-        self.assertIsNotNone( actionstore )
+        self.assertIsNotNone( actionstore, "Install action object exists" )
 
     def test_Can_retrieve_action_object_by_store( self ):
         actionobj = actions.get( 'install' )
         actionstore = M.Action.objects.get( name='install' )
-        self.assertIsNotNone( actionobj )
-        self.assertIsNotNone( actionstore )
-        self.assertIs( actionobj, Action.get_for_object( actionstore ))
+        self.assertIsNotNone( actionobj, "Install action exists" )
+        self.assertIsNotNone( actionstore, "Install action object exists" )
+        self.assertIs( actionobj, Action.get_for_object( actionstore ),
+                       "Action object retrieved directly is same as action "
+                       "object retrieved by store" )
+
+    def test_Core_apps_automatically_installed( self ):
+        required = M.Action.objcects.filter( required=True )
+        notinstalled = required.filter( installed=False )
+        print 'required apps', required.all()
+        self.assertGreater( required.count(), 0,
+                            "Required applications exist" )
+        self.assertFalse( notinstalled.count(),
+                          "All required appliactions are installed" )

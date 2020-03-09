e25b6d3ffbbf2deb35bf40dd9fdd3a12843716ce
Ponder Stibbons <theponderstibbons@gmail.com>
2020-03-08 19:51:09 -0500

    Remove test db from settings

--- src/pantologic/settings.py
+++ src/pantologic/settings.py
@@ -160,10 +160,6 @@ DATABASES = {
         'ENGINE'   : 'django.db.backends.postgresql',
         'NAME'     : 'pantologic',
     },
-    'test': {
-        'ENGINE'   : 'django.db.backends.postgresql',
-        'NAME'     : 'test_pantologic',
-    },
     # 'fallback': {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
